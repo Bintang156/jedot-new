f46fb8902cce36c158e2f65e8d58324c3d9e08d9
Bintang156 <adventureofalifetime@webmail.umm.ac.id>
2021-06-25 13:13:49 +0800

    initial release

---
+++ resources/views/template/layout.blade.php
@@ -0,0 +1,148 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Jedot</title>
+        <link rel="icon" type="image/x-icon" href="img/favicon.ico" />
+        <script src="https://use.fontawesome.com/releases/v5.15.1/js/all.js" crossorigin="anonymous"></script>
+        <link href="https://fonts.googleapis.com/css?family=Merriweather+Sans:400,700" rel="stylesheet" />
+        <link href="https://fonts.googleapis.com/css?family=Merriweather:400,300,300italic,400italic,700,700italic" rel="stylesheet" type="text/css" />
+        <link href="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/magnific-popup.min.css" rel="stylesheet" />
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+    <body id="page-top">
+        
+        <nav class="navbar navbar-expand-lg navbar-light fixed-top py-3" id="mainNav">
+            <div class="container">
+                <a class="navbar-brand js-scroll-trigger" href="#page-top">Jedot </a>
+                <button class="navbar-toggler navbar-toggler-right" type="button" data-toggle="collapse" data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+                    <ul class="navbar-nav ml-auto my-2 my-lg-0">
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#services">Layanan</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#portfolio">Portofolio</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#contact">Kontak</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="tol">Login</a></li>
+                        <li class="nav-item"><a class="nav-link js-scroll-trigger" href="admin">Admin</a></li>
+                    </ul>
+                </div>
+            </div>
+        </nav>
+        <header class="masthead">
+            <div class="container h-100">
+                <div class="row h-100 align-items-center justify-content-center text-center">
+                    <div class="col-lg-10 align-self-end">
+                        <h1 class="text-uppercase text-white font-weight-bold">Sesuaikan poto dan video sesuai seleramu disini</h1>
+                    </div>
+                    <div class="col-lg-8 align-self-baseline"></div>
+                        <p class="text-white-75 font-weight-light mb-5">Perusahaan yang berjalan dibidang jasa multimedia mulai dari editing video, foto, template foto / video dan masih banyak yang lainnya</p>
+                    </div>
+                </div>
+            </div>
+        </header>
+        <section class="page-section" id="services">
+            <div class="container">
+                <h2 class="text-center mt-0">Apa Saja layanan yang ada ?</h2>
+                <hr class="divider my-4" />
+                <div class="row">
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-4x fa-film text-primary mb-4"></i>
+                            <h3 class="h4 mb-2">Video</h3>
+                            <p class="text-muted mb-0">Kami bisa membuat konsep dan editing video sesuai dengan keinginanmu.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-4x fa-camera text-primary mb-4"></i>
+                            <h3 class="h4 mb-2">Photo</h3>
+                            <p class="text-muted mb-0">Kami bisa membuat konsep dan editing Foto sesuai dengan keinginanmu.</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-4x fa-globe text-primary mb-4"></i>
+                            <h3 class="h4 mb-2">Template Photo / Video</h3>
+                            <p class="text-muted mb-0">Anda bisa membeli dan menggunakan video dan Foto yang ada di website kami</p>
+                        </div>
+                    </div>
+                    <div class="col-lg-3 col-md-6 text-center">
+                        <div class="mt-5">
+                            <i class="fas fa-4x fa-window-restore text-primary mb-4"></i>
+                            <h3 class="h4 mb-2">Graphic Design</h3>
+                            <p class="text-muted mb-0">buat 3d atau animasi sesuai keinginanmu</p>
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <div id="portfolio">
+            <div class="container-fluid p-0">
+                <div class="row no-gutters">
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="img/portfolio/fullsize/1.jpg">
+                            <img class="img-fluid" src="img/portfolio/thumbnails/1.jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Indonesia</div>
+                                <div class="project-name">Bendera Indonesia</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="img/portfolio/fullsize/2.jpg">
+                            <img class="img-fluid" src="img/portfolio/thumbnails/2.jpg" alt="" />
+                            <div class="portfolio-box-caption">
+                                <div class="project-category text-white-50">Indonesia</div>
+                                <div class="project-name">Bali</div>
+                            </div>
+                        </a>
+                    </div>
+                    <div class="col-lg-4 col-sm-6">
+                        <a class="portfolio-box" href="img/portfolio/fullsize/3.jpg">
+                            <img class="img-fluid" src="img/portfolio/thumbnails/3.jpg" alt="" />
+                            <div class="portfolio-box-caption p-3">
+                                <div class="project-category text-white-50">Indonesia</div>
+                                <div class="project-name">Pasar</div>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+            </div>
+        </div>
+        <section class="page-section" id="contact">
+            <div class="container">
+                <div class="row justify-content-center">
+                    <div class="col-lg-8 text-center">
+                        <h2 class="mt-0">Kontak Kami</h2>
+                        <hr class="divider my-4" />
+                        <p class="text-muted mb-5">apakah anda siap untuk membuat project dengan kami? kontak kami melalui :
+                            <li>Rachmaddillah Ibnu Bintang - 201910370311156</li>
+                            <li>Muhamad Yoga Zikri Saputra - 201910370311157</li>
+                            <li>Mulyono Rahuld Mangesak    - 201910370311174</li>
+                        </p>
+                    </div>
+                </div>
+                <div class="row">
+                    <div class="col-lg-4 ml-auto text-center mb-5 mb-lg-0">
+                        <i class="fas fa-phone fa-3x mb-3 text-muted"></i>
+                        <div>+62 0851-5151-6666</div>
+                    </div>
+                    <div class="col-lg-4 mr-auto text-center">
+                        <i class="fas fa-envelope fa-3x mb-3 text-muted"></i>
+                        <a class="d-block" href="mailto:jedot@gmail.com">jedot@gmail.com</a>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <footer class="bg-light py-5">
+            <div class="container"><div class="small text-center text-muted">Copyright © 2021 - Jedot</div></div>
+        </footer>
+        <script src="https://code.jquery.com/jquery-3.5.1.slim.min.js"></script>
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/js/bootstrap.bundle.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery-easing/1.4.1/jquery.easing.min.js"></script>
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/magnific-popup.js/1.1.0/jquery.magnific-popup.min.js"></script>
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
---
+++ routes/web.php
@@ -0,0 +1,36 @@
+<?php
+
+use App\Http\Controllers\control_authors;
+use App\Http\Controllers\Control_Home;
+use App\Http\Controllers\control_news;
+use App\Http\Controllers\tolController;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+Route::get('/', [Control_Home::class, 'index']);
+
+route::get('tol', [tolController::class, 'index']);
+
+
+Route::get('/admin', [control_authors::class, 'index']);
+Route::resource('admin', control_authors::class);
+Route::get('/gambar/{image}', [control_authors::class, 'load_image']);
+Route::get('/hapus/{id}', [control_authors::class, 'destroy']);
+Route::get('/edit/{id}', [control_authors::class, 'edit']);
+Route::get('/update/{id}', [control_authors::class, 'update']);
+
+Route::get('/news', [control_news::class, 'index']);
+Route::resource('news', control_news::class);
+Route::get('/gambar/news/{image}', [control_news::class, 'load_image']);
+Route::get('/hapus/news/{id}', [control_news::class, 'destroy']);
+
+

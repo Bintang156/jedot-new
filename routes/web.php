ada54b728d8c229bf259158264964aacbee8f22e
Bintang156 <79825193+Bintang156@users.noreply.github.com>
2021-06-25 14:20:22 +0800

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
 use App\Http\Controllers\control_authors;
-use App\Http\Controllers\Control_Home;
+use App\Http\Controllers\Control_home;
 use App\Http\Controllers\control_news;
 use App\Http\Controllers\tolController;
 use Illuminate\Support\Facades\Route;
